88984f0a1dd03c4543c95e5996e22a6b4391c249
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-06-27 23:46:52 -0500

    Add hello word and dev dependencies

---
+++ app/index.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// Components have
+// UI
+// state
+// lifecycle event
+
+// component definition
+class App extends React.Component {
+    render() {
+        return (
+        <div> 
+            Hello World!
+        </div>
+        )
+    }
+}
+
+// component invocation
+ReactDOM.render(
+    <App />,
+    document.getElementById('app');
+);
