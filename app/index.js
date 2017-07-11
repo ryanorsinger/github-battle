07f167412fa994f5f6488ed99d2e1d7314111eea
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-07-11 15:49:34 -0500

    Add Users component to list friends/non-friends

--- app/index.js
+++ app/index.js
@@ -8,6 +8,52 @@ require('./index.css');
 // UI, always UI
 // many have state
 // often some kind of lifecycle event
+var userList = [
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false }
+];
+
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function(person) {
+        return person.friend === true;
+    });
+    var notFriends = this.props.list.filter(function(person) {
+       return person.friend === false;
+    });
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          { friends.map(function(person) {
+            return <li key={person.name}>{person.name}</li>
+          })}
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          { notFriends.map(function(person) {
+            return <li key={person.name}>{person.name}</li>
+          })}
+        </ul>        
+      </div>
+    )
+  }
+}
+
+Users.propTypes = {
+    list: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        friend: PropTypes.bool.isRequired,
+    }))
+}
+
 
 class Avatar extends React.Component {
     render() {
@@ -33,6 +79,7 @@ class Badge extends React.Component {
                <Avatar img={this.props.img} />
                <Label name={this.props.name} />
                <ScreenName username={this.props.username} />
+               <Users list={this.props.list} />
             </div>
         )
     }
@@ -45,11 +92,14 @@ Badge.propTypes = {
 }
 
 
+
+
 ReactDOM.render(
     <Badge
         name='Ryan Orsinger'
         username='ryanorsinger'
         img='https://avatars3.githubusercontent.com/u/5657581?v=3&s=460'
+        list={userList}
     />,
     document.getElementById('app')
 );
