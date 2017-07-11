68a91f5dfed3030b5db8c49ff045ed67914d0a82
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-07-11 14:38:06 -0500

    Add nested components for badge, avatar, label, and screenName

--- app/index.js
+++ app/index.js
@@ -2,24 +2,46 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
+const USER_DATA = {
+    img: 'https://avatars3.githubusercontent.com/u/5657581?v=3&s=460',
+    name: 'Ryan Orsinger',
+    username: 'ryanorsinger'
+}
 // Components have
 // UI, always UI
 // many have state
 // often some kind of lifecycle event
 
-// component definition
-class App extends React.Component {
+class Avatar extends React.Component {
+    render() {
+        return <img src={this.props.img} />
+    }
+}
+class Label extends React.Component {
+    render() {
+        return <h1>Name: {this.props.name} </h1>
+    }
+}
+
+class ScreenName extends React.Component {
+    render() {
+        return <h3>Username: {this.props.username}</h3>
+    }
+}
+
+class Badge extends React.Component {
     render() {
         return (
-        <div>
-            Hello Everybody!
-        </div>
+            <div>
+               <Avatar img={this.props.user.img} />
+               <Label name={this.props.user.name} />
+               <ScreenName username={this.props.user.username} />
+            </div>
         )
     }
 }
 
-// component invocation
 ReactDOM.render(
-    <App />,
+    <Badge user={USER_DATA}/>,
     document.getElementById('app')
 );
